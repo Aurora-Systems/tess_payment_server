b90972af97acc192f5669d4366ab91bd63688170
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2024-10-13 13:28:11 +0200

    fix error

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 // import files
 require('dotenv').config()
-const cors = requires("cors")
+const cors = require("cors")
 const express = require("express")
 const serverless = require("serverless-http")
 const MobileMoney = require("./payment_types/mobile_money")
@@ -13,7 +13,7 @@ app.use(cors({
 }))
 app.use(express.json())
 
-const route = express.Router();
+const route = express.Router(); 
 
 route.get('/',(req,res)=>{
     res.send('<a href="https://bleu.aurorasystems.co.zw">Bleu Finance Payment Solutions</a>')
